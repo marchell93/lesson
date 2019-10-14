4b6159b524d692b10e8a56ee3f333dfac8d65ff9
Марк Петрович <marchell93@mail.ru>
2019-10-14 22:00:38 +0300

    Исправил, спасибо за замечания.

--- lesson_003/07_wall.py
+++ lesson_003/07_wall.py
@@ -9,22 +9,14 @@ import simple_draw as sd
 count = 1
 # Цикл считающий координату y
 for y in range(0, 600, 50):
-    # TODO Вот тут комментарий абсолютно оправдан)
     # Условие необходимое для определения какой ряд кирпичей мы кладем
-    # TODO Видно, что сам цикл в обоих условиях одинаковый, отличается только начальное значение диапазона.
-    #  Значит можно в условиях задать его, а цикл уже написать один раз после условий
     if count % 2 != 0:
-        for x in range(-50, 600, 100):
-            point_start_first = sd.get_point(x, y)
-            point_finish_first = sd.get_point(x + 100, y + 50)
-            sd.rectangle(point_start_first, point_finish_first, width=3)
-    elif count % 2 == 0:
-        for x in range(0, 600, 100):
-            point_start_first = sd.get_point(x, y)
-            point_finish_first = sd.get_point(x + 100, y + 50)
-            sd.rectangle(point_start_first, point_finish_first, width=3)
-    # TODO Такая запись явно сведетельствует, что нужно было предыдущий elif сдалать else)
+        x_range = -50
     else:
-        pass
+        x_range = 0
+    for x in range(x_range, 600, 100):
+        point_start_first = sd.get_point(x, y)
+        point_finish_first = sd.get_point(x + 100, y + 50)
+        sd.rectangle(point_start_first, point_finish_first, width=3)
     count += 1
 sd.pause()
