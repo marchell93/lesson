f50aa62eaf8173535cfdd7f2b25a97a5af847560
Марк Петрович <marchell93@mail.ru>
2019-11-09 11:53:36 +0300

    Готово!!!

--- lesson_005/01_inhabitants.py
+++ lesson_005/01_inhabitants.py
@@ -3,6 +3,13 @@
 # Вывести на консоль жителей комнат (модули room_1 и room_2)
 # Формат: В комнате room_1 живут: ...
 
-# TODO здесь ваш код
+import room_1, room_2
 
 
+def room_residents(residents):
+    for resident in residents.folks:
+        print(f"В комнате {residents.__name__} живет: {resident}")
+
+
+room_residents(room_1)
+room_residents(room_2)
