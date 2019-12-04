a76b5a6ebdf8e261bd51495967ccb38e4ced3372
Марк Петрович <marchell93@mail.ru>
2019-12-04 20:14:07 +0300

    Готово!!! Спасибо большое за замечания!!! Они помогают лучше усваивать материал!!!

--- lesson_005/02_district.py
+++ lesson_005/02_district.py
@@ -5,26 +5,16 @@
 # подсказка: для вывода элементов списка через запятую можно использовать функцию строки .join()
 # https://docs.python.org/3/library/stdtypes.html#str.join
 
-from lesson_005.district.central_street.house1.room1 import folks as cen_str_h1r1
-from lesson_005.district.central_street.house1.room2 import folks as cen_str_h1r2
-from lesson_005.district.central_street.house2.room1 import folks as cen_str_h2r1
-from lesson_005.district.central_street.house2.room2 import folks as cen_str_h2r2
-from lesson_005.district.soviet_street.house1.room1 import folks as sov_str_h1r1
-from lesson_005.district.soviet_street.house1.room2 import folks as sov_str_h1r2
-from lesson_005.district.soviet_street.house2.room1 import folks as sov_str_h2r1
-from lesson_005.district.soviet_street.house2.room2 import folks as sov_str_h2r2
+from district.central_street.house1.room1 import folks as cen_str_h1r1
+from district.central_street.house1.room2 import folks as cen_str_h1r2
+from district.central_street.house2.room1 import folks as cen_str_h2r1
+from district.central_street.house2.room2 import folks as cen_str_h2r2
+from district.soviet_street.house1.room1 import folks as sov_str_h1r1
+from district.soviet_street.house1.room2 import folks as sov_str_h1r2
+from district.soviet_street.house2.room1 import folks as sov_str_h2r1
+from district.soviet_street.house2.room2 import folks as sov_str_h2r2
 
-# TODO А зачем lesson_005 при импорте? Пакет district лежин на том же уровне, что и этот модуль,
-#  так что просто from district. ...
-
-# TODO Можно здесь в один список всех положить -
-#  all_people = cen_str_h1r1 + cen_str_h1r2 + ...
-all_people = [cen_str_h1r1, cen_str_h1r2, cen_str_h2r1, cen_str_h2r2, sov_str_h1r1,
-                          sov_str_h1r2, sov_str_h2r1, sov_str_h2r2]
-all_people_in_district = []
-for people_in_district in all_people:
-    for peoples in people_in_district:
-        all_people_in_district.append(peoples)
-# TODO И сразу этот список через join пропускать
-all_people_in_district_string = ', '.join(all_people_in_district)
+all_people = cen_str_h1r1 + cen_str_h1r2 + cen_str_h2r1 + cen_str_h2r2 + sov_str_h1r1 + \
+             sov_str_h1r2 + sov_str_h2r1 + sov_str_h2r2
+all_people_in_district_string = ', '.join(all_people)
 print(f'На районе живут: {all_people_in_district_string}')
