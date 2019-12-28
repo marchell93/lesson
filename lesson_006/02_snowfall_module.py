f82c1be219080608d024338ca20f2e1894fcefd0
Марк Петрович <marchell93@mail.ru>
2019-12-28 22:34:46 +0300

    Готово!!! Посмотрите пожалуйста!!!

--- lesson_006/02_snowfall_module.py
+++ lesson_006/02_snowfall_module.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import simple_draw as sd
+from snowfall import create_snowflake, draw_snowflake_color, move_snowflake, \
+    count_snowflake_bottom_of_screen, delete_snowflake, create_snowflake_modified
 
 # На основе кода из lesson_004/05_snowfall.py
 # сделать модуль snowfall.py в котором реализовать следующие функции
@@ -13,14 +15,25 @@ import simple_draw as sd
 # В текущем модуле реализовать главный цикл падения снежинок,
 # обращаясь ТОЛЬКО к функциям модуля snowfall
 
+number_of_snowflake = 20
 # создать_снежинки(N)
+x, y, length_of_snowflake = create_snowflake(number_of_snowflake)
 while True:
+    sd.start_drawing()
     #  нарисовать_снежинки_цветом(color=sd.background_color)
+    draw_snowflake_color(number_of_snowflake, x, y, length_of_snowflake, sd.background_color)
     #  сдвинуть_снежинки()
+    move_snowflake(number_of_snowflake, x, y)
     #  нарисовать_снежинки_цветом(color)
+    draw_snowflake_color(number_of_snowflake, x, y, length_of_snowflake, sd.COLOR_WHITE)
     #  если есть номера_достигших_низа_экрана() то
-    #       удалить_снежинки(номера)
-    #       создать_снежинки(count)
+    count_snowflake_bottom_of_scr = count_snowflake_bottom_of_screen(number_of_snowflake, y, length_of_snowflake)
+    if count_snowflake_bottom_of_scr:
+        # удалить_снежинки(номера)
+        delete_snowflake(count_snowflake_bottom_of_scr, x, y, length_of_snowflake)
+        # создать_снежинки(count)
+        create_snowflake_modified(count_snowflake_bottom_of_scr, x, y)
+    sd.finish_drawing()
     sd.sleep(0.1)
     if sd.user_want_exit():
         break
---
+++ lesson_006/snowfall.py
@@ -0,0 +1,47 @@
+import simple_draw as sd
+
+X = []
+Y = []
+LENGTH_of_SNOWFLAKE = []
+
+
+def create_snowflake(number_of_snowflake):
+    for _ in range(number_of_snowflake):
+        X.append(sd.random_number(0, 600))
+        Y.append(sd.random_number(300, 650))
+        LENGTH_of_SNOWFLAKE.append(sd.random_number(10, 50))
+    return X, Y, LENGTH_of_SNOWFLAKE
+
+
+def draw_snowflake_color(number_of_snowflake, x, y, length_of_snowflake, color):
+    for i in range(number_of_snowflake):
+        point_0 = sd.get_point(x[i], y[i])
+        sd.snowflake(center=point_0, length=length_of_snowflake[i], color=color)
+
+
+def move_snowflake(number_of_snowflake, x, y):
+    for i in range(number_of_snowflake):
+        y[i] -= 5
+        x[i] += sd.random_number(-5, 5)
+
+
+def count_snowflake_bottom_of_screen(number_of_snowflake, y, length_of_snowflake):
+    count_bottom_of_scr = []
+    for i in range(number_of_snowflake):
+        if y[i] < length_of_snowflake[i]:
+            count_bottom_of_scr.append(i)
+    return count_bottom_of_scr
+
+
+def delete_snowflake(count_bottom_of_scr, x, y, length_of_snowflake):
+    for count in count_bottom_of_scr:
+        point_0 = sd.get_point(x[count], y[count])
+        sd.snowflake(center=point_0, length=length_of_snowflake[count], color=sd.background_color)
+        x[count] = 0
+        y[count] = 0
+
+
+def create_snowflake_modified(count_bottom_of_scr, x, y):
+    for count in count_bottom_of_scr:
+        x[count] = sd.random_number(0, 600)
+        y[count] = sd.random_number(600, 650)
