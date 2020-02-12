ff6ae9e2792d5a9752e4237934241f418f8b1477
Марк Петрович <marchell93@mail.ru>
2020-02-13 00:57:34 +0300

    Второе задание готово!!! Можно проверять!!! ООП - маст хэв!!!)))

--- lesson_007/02_alchemy.py
+++ lesson_007/02_alchemy.py
@@ -19,7 +19,141 @@
 #   print(Water(), '+', Air(), '=', Water() + Air())
 #   print(Fire(), '+', Air(), '=', Fire() + Air())
 
-# TODO здесь ваш код
+
+class Water:
+
+    def __init__(self):
+        self.obj = 'Вода'
+
+    def __str__(self):
+        return f'{self.obj}'
+
+    def __add__(self, other):
+        if other.obj == 'Воздух':
+            return Storm()
+        elif other.obj == 'Огонь':
+            return Vapor()
+        elif other.obj == 'Земля':
+            return Dirt()
+        else:
+            return None
+
+
+class Air:
+
+    def __init__(self):
+        self.obj = 'Воздух'
+
+    def __str__(self):
+        return f'{self.obj}'
+
+    def __add__(self, other):
+        if other.obj == 'Огонь':
+            return Thunderbolt()
+        elif other.obj == 'Земля':
+            return Dust()
+        elif other.obj == 'Вода':
+            return Storm()
+        else:
+            return None
+
+
+class Fire:
+
+    def __init__(self):
+        self.obj = 'Огонь'
+
+    def __str__(self):
+        return f'{self.obj}'
+
+    def __add__(self, other):
+        if other.obj == 'Земля':
+            return Lava()
+        elif other.obj == 'Вода':
+            return Vapor()
+        elif other.obj == 'Воздух':
+            return Thunderbolt()
+        else:
+            return None
+
+
+class Ground:
+
+    def __init__(self):
+        self.obj = 'Земля'
+
+    def __str__(self):
+        return f'{self.obj}'
+
+    def __add__(self, other):
+        if other.obj == 'Вода':
+            return Dirt()
+        elif other.obj == 'Огонь':
+            return Lava()
+        else:
+            return None
+
+
+class Storm:
+
+    def __init__(self):
+        self.obj = 'Шторм'
+
+    def __str__(self):
+        return f'{self.obj}'
+
+
+class Vapor:
+
+    def __init__(self):
+        self.obj = 'Пар'
+
+    def __str__(self):
+        return f'{self.obj}'
+
+
+class Dirt:
+
+    def __init__(self):
+        self.obj = 'Грязь'
+
+    def __str__(self):
+        return f'{self.obj}'
+
+
+class Thunderbolt:
+
+    def __init__(self):
+        self.obj = 'Молния'
+
+    def __str__(self):
+        return f'{self.obj}'
+
+
+class Dust:
+
+    def __init__(self):
+        self.obj = 'Пыль'
+
+    def __str__(self):
+        return f'{self.obj}'
+
+
+class Lava:
+
+    def __init__(self):
+        self.obj = 'Лава'
+
+    def __str__(self):
+        return f'{self.obj}'
+
+
+print(f'{Water()} + {Air()} = {Water() + Air()}')
+print(f'{Water()} + {Fire()} = {Water() + Fire()}')
+print(f'{Water()} + {Ground()} = {Water() + Ground()}')
+print(f'{Air()} + {Fire()} = {Air() + Fire()}')
+print(f'{Air()} + {Ground()} = {Air() + Ground()}')
+print(f'{Fire()} + {Ground()} = {Fire() + Ground()}')
 
 # Усложненное задание (делать по желанию)
 # Добавить еще элемент в игру.
