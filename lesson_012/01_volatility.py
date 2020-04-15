c9ed58bc71750585d762a72bb85f92bce81ca5b1
Марк Петрович <marchell93@mail.ru>
2020-04-15 23:07:13 +0300

    Первое задание готово, проверьте пожалуйста!

--- lesson_012/01_volatility.py
+++ lesson_012/01_volatility.py
@@ -72,5 +72,65 @@
 #
 #     def run(self):
 #         <обработка данных>
+import os
 
-# TODO написать код в однопоточном/однопроцессорном стиле
+
+class Volatility:
+    total_volatility = {}
+    null_volatility = []
+
+    def __init__(self, pathfile, filename):
+        self.filepath = os.path.join(pathfile, filename)
+        self.filename = filename
+
+    def run(self):
+        self.open_file()
+        Volatility.total_volatility = dict(sorted(self.total_volatility.items(), key=lambda pair: pair[1], reverse=True))
+
+    def open_file(self):
+        total_price = []
+        with open(self.filepath, mode='r', encoding='utf-8') as file:
+            for line in file:
+                data_from_file = line[:-1].split(',')
+                try:
+                    if float(data_from_file[2]):
+                        total_price.append(float(data_from_file[2]))
+                except ValueError:
+                    print(f'Первая строка файла - заголовок... читаем дальше')
+            self.calculation_volatility(total_price)
+
+    def calculation_volatility(self, total_price):
+        max_price = max(total_price)
+        min_price = min(total_price)
+        average_price = (max_price + min_price) / 2
+        volatility = ((max_price - min_price) / average_price) * 100
+        if volatility == 0.0:
+            self.null_volatility.append(self.filename.strip('.csv'))
+        else:
+            self.total_volatility[self.filename.strip('.csv')] = volatility
+
+    def print_null_volatility(self):
+        konv_to_str = ', '.join(self.null_volatility)
+        print(f'Нулевая волатильность:\n{konv_to_str}')
+
+    def print_total_volatility(self):
+        count = 0
+        len_tot_vol = len(self.total_volatility)
+        print(f'Максимальная волатильность:')
+        for key, value in self.total_volatility.items():
+            count += 1
+            if 3 >= count >= 1:
+                print(f'{key} - {round(value, 3)} %')
+            elif count == 4:
+                print(f'Минимальная волатильность:')
+            elif len_tot_vol - 3 <= count <= len_tot_vol - 1:
+                print(f'{key} - {round(value, 3)} %')
+
+
+if __name__ == '__main__':
+    for dir, subdir, filenames in os.walk('trades'):
+        volatilitys = [Volatility(dir, filename) for filename in filenames]
+        for vol in volatilitys:
+            vol.run()
+        vol.print_total_volatility()
+        vol.print_null_volatility()
